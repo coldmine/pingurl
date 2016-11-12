fdebb9a01e921a4d3eb67d1f8e25fc350c67ac5f
kimhanwoong <khw7096@gmail.com>
2016-11-12 10:58:01 +0900

    명령어 이름 변경

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,3 +1,3 @@
-GOOS=linux GOARCH=amd64 go build -o ./bin/linux/pingurl pingurl.go
-GOOS=darwin GOARCH=amd64 go build -o ./bin/osx/pingurl pingurl.go
-GOOS=windows GOARCH=amd64 go build -o ./bin/windows/pingurl.exe pingurl.go
+GOOS=linux GOARCH=amd64 go build -o ./bin/linux_x86_64/urlcheck urlcheck.go
+GOOS=darwin GOARCH=amd64 go build -o ./bin/macos_x86_64/urlcheck urlcheck.go
+GOOS=windows GOARCH=amd64 go build -o ./bin/windows_x86_64/urlcheck.exe urlcheck.go
