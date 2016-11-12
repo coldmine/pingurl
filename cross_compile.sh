63447ad2e0d8bade1b6244a8d2f00825fe1d8419
kim yongbin <kybinz@gmail.com>
2016-11-12 12:20:43 +0900

    bin 디렉토리 안에 빌드 파일 생성
    제가 제안했던것은 각각의 파일을 따로 올리는 거였어요.
    대신 각각의 파일이 파일명이 달라야 해서
    각 아키텍쳐 디렉토리안에 파일이 들어가는것이 아니라
    파일에 아키텍쳐명이 붙는 형식으로 고쳐봤어요.
    
    압축은 하지 않았어요. 압축 푸는것보다 이름을 고쳐서 쓰는게
    더 편한 방법이라고 생각했어요.
    
    마지막으로 bin안에 있던 바이너리 파일은 지웠어요.
    중복인것 같아서요.

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,3 +1,3 @@
-GOOS=linux GOARCH=amd64 go build -o ./bin/linux_x86_64/urlcheck urlcheck.go
-GOOS=darwin GOARCH=amd64 go build -o ./bin/macos_x86_64/urlcheck urlcheck.go
-GOOS=windows GOARCH=amd64 go build -o ./bin/windows_x86_64/urlcheck.exe urlcheck.go
+GOOS=linux GOARCH=amd64 go build -o ./bin/urlcheck_linux_x86_64
+GOOS=darwin GOARCH=amd64 go build -o ./bin/urlcheck_macos_x86_64
+GOOS=windows GOARCH=amd64 go build -o ./bin/urlcheck_windows_x86_64.exe
